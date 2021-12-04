2cb0d75fb56063cc3b6588e9cb8d63473939e007
hermanvulkers <hermanvulkers@gmail.com>
2021-12-04 13:50:21 +0100

    Completed dark mode switcher

---
+++ themeswitcher.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("#checkbox");
+const body = document.body;
+
+function change() {
+	btn.checked ? body.classList.add("dark") : body.classList.remove("dark");
+}
+
+btn.addEventListener("change", change);
